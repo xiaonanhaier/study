00f14a47045e89039b93f452bcc3e572170bd4fd
Fang <muguner@163.com>
2017-04-05 15:27:46 +0800

    精简了js

--- project/pc-水果电商首页/js/index.js
+++ project/pc-水果电商首页/js/index.js
@@ -1,15 +1,20 @@
   window.onload = function () {
     var bign = document.getElementById('img');
-    bign.style.background ='url(images/long.png) center center no-repeat'
-    console.log(bign.style);
+    bign.style.background ='url(images/long0.png) center center no-repeat'
+    // console.log(bign.style);
     fade(bign,0);
+    var time = 0;
     var timeid = setInterval(function() {
-      fade(bign,0);
-      if (bign.style.background == 'url("images/long.png") 50% 50% no-repeat') {
-        bign.style.background ='url(images/long1.png) 50% 50% no-repeat';
-      }else {
-        bign.style.background = 'url(images/long.png) 50% 50% no-repeat';
+      bign.style.background ='url(images/long'+time+'.png) 50% 50% no-repeat';
+      time+=1;
+      if (time > 1) {
+        time =0;
       }
+      // if (bign.style.background == 'url("images/long.png") 50% 50% no-repeat') {
+      //   bign.style.background ='url(images/long1.png) 50% 50% no-repeat';
+      // }else {
+      //   bign.style.background = 'url(images/long.png) 50% 50% no-repeat';
+      // }
     },3000)
   }
   function getStyle(obj, attr){
@@ -21,6 +26,7 @@
   }
   function fade(obj, nNum){
     clearInterval(obj.timeId);
+    // console.log('ssss');
     var speed = 0;
     //通过speed 正负控制方向
     // console.log(getStyle(obj, 'opacity'));
@@ -34,10 +40,10 @@
       var opacity = getStyle(obj, 'opacity');
       obj.style.opacity = Number(opacity) + speed;
           var result = nNum - getStyle(obj, 'opacity');
-          // console.log(result);
       if(Math.abs(result) <= 0.01){
           clearInterval(obj.timeId);
-          fade(obj,getStyle(obj, 'opacity'))
+          var aa = 1-nNum;
+          fade(obj,aa)
           // obj.style.opacity  = nNum;
       }
     }, 30);
