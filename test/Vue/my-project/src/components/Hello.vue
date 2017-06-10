946eda54c29c350a810112fdb410dacbe4d2ee16
Fang <muguner@163.com>
2017-06-10 17:58:41 +0800

    vue 使用

--- test/Vue/my-project/src/components/Hello.vue
+++ test/Vue/my-project/src/components/Hello.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2><input v-model="inp" type="text" @keyup.enter='aa()'></h2>
+    <ol>
+      <li v-for="item in items" v-text="item.name"  :class="['co',{'aa':item.bg}]" v-on:click="tan(item)"></li>
+    </ol>
   </div>
 </template>
-
 <script>
+import Stors from '../stors'
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      inp:'',
+      msg: 'Welcome to Your Vue.js App',
+      items:Stors.fetch()
+    }
+  },
+  methods:{
+    tan:function(aa) {
+      alert(aa.name);
+    },
+    aa:function() {
+      this.items.splice(0,0,{
+        'name':this.inp,
+        'age':24,
+        'bg':true
+      })
+      this.inp = ''
+    }
+  },
+  watch:{
+    items:{
+      handler:function() {
+        Stors.save(this.items);
+      },
+      deep:true
     }
   }
 }
@@ -37,17 +48,21 @@ h1, h2 {
   font-weight: normal;
 }
 
-ul {
+/*ul {
   list-style-type: none;
   padding: 0;
-}
+}*/
 
 li {
-  display: inline-block;
   margin: 0 10px;
+  width: 30%;
+  margin: 0 auto;
+  text-align: center;
 }
-
-a {
+.co{
+  color: red;
+}
+.aa{
   color: #42b983;
 }
 </style>
