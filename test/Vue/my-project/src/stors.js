2b9ba38447cb045814d5581b0dda12aa134d5d2e
fang <muguner@163.com>
2017-10-25 16:17:55 +0800

    创建vue开发框架

--- test/Vue/my-project/src/stors.js
+++
@@ -1,9 +0,0 @@
-const STOPAGE_KEY  = 'chenxi'
-export default{
-  fetch:function() {
-    return JSON.parse(window.localStorage.getItem(STOPAGE_KEY) || '[]');
-  },
-  save:function(items) {
-    window.localStorage.setItem(STOPAGE_KEY,JSON.stringify(items));
-  }
-}
